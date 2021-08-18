a5a7e2cebad5c81f5cc456353e5c762dd6c08afe
BrunoBig <bnbigliatto@gmail.com>
2021-08-18 17:58:01 -0300

    variables

---
+++ infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "sa-east-1"
+}
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
+}
+
+variable "key_pair_name" {
+  default = "key-consolepolicy-3"
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-a04538e9"
+}
+
+variable "vpc_id" {
+  default = "vpc-753bf913"
+}
